7a370687754b58e895f42ebe3c25769b32f3b87a
ari pratomo <ari633@gmail.com>
2019-02-10 02:22:47 +0700

    Run with asyncio

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from urllib import request, error
 from bs4 import BeautifulSoup
-import datetime
+import asyncio
 import json
 
 
 class LirikLagu:
     """Crawling artist and song lyrics from https://lirik.kapanlagi.com"""
-    data = {}
 
     @staticmethod
     def alphabet_link():
@@ -37,44 +36,56 @@ class LirikLagu:
         song = soup.find('div', {'class': 'lyrics-body'}).find_all(["span", "br"])
         return song
 
-    def push_artist_song(self, artist_name, url_artist):
+    def get_song(self, url):
+        artist_songs = {}
+        soup = self.soup_in_bowl(url)
+        songs = soup.find('ul', {'id': 'lyric-centerlist'}).find_all('a')
+        for song in songs:
+            song_url = song.get('href')
+            song_title = song.text
+            text_song = ""
+            detail_songs = self.get_detail_song(song_url)
+            for detail_song in detail_songs:
+                if detail_song.string is not None:
+                    text_song += "\n"
+                    text_song += detail_song.text
+                else:
+                    text_song += "\n"
+            if song_title:
+                artist_songs[song_title] = {"title": song_title, "source": song_url, "lyrics": text_song}
+
+        return artist_songs
+
+    async def get_artist_song(self, url_artist):
+        print(url_artist)
+        artist_songs = {"songs": {}}
         pages = self.get_pagination_link(url_artist, {'class': 'col-lirik pagination'})
         if pages:
-            print(pages)
+            for url_page in pages:
+                artist_songs["songs"].update(self.get_song(url_page))
         else:
-            soup = self.soup_in_bowl(url_artist)
-            songs = soup.find('ul', {'id': 'lyric-centerlist'}).find_all('a')
-            for song in songs:
-                song_url = song.get('href')
-                song_title = song.text
-                text_song = ""
-                detail_songs = self.get_detail_song(song_url)
-                for detail_song in detail_songs:
-                    if detail_song.string != None :
-                        text_song += "\n"
-                        text_song += detail_song.text
-                    else:
-                        text_song += "\n"
-                print(artist_name)
-                print(song_url)
-                print(song_title)
-                print(text_song)
+            artist_songs["songs"].update(self.get_song(url_artist))
+        return artist_songs
 
-    def scrappy(self):
+    async def scrappy(self):
         pages = self.get_pagination_link(self.alphabet_link(), {'class': 'col-lirik pagination2'})
+        data = {}
         for page in pages:
             soup = self.soup_in_bowl(page)
             artists = soup.find_all('div', {'class': 'div-horizontal2-list'})
             for artist in artists:
                 artist_name = artist.a.text
                 artist_url = artist.a.get('href')
-                self.data[artist_name] = {'url': artist_url}
-                self.push_artist_song(artist_name, artist_url)
+                result = await self.get_artist_song(artist_url)
+                data[artist_name] = {"results": result}
+        return data
 
 
-def main():
-    lirikLagu = LirikLagu()
-    lirikLagu.scrappy()
+async def main():
+    song_lyrics = LirikLagu()
+    song_results = await song_lyrics.scrappy()
+    with open('index.json', 'w') as file_json:
+        json.dump(song_results, file_json)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
